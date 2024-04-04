7657bd649f83fb413445fbc6ecae29f35fec19fa
TahaAliAbbasi <163841832+TahaAliAbbasi@users.noreply.github.com>
2024-04-04 11:56:28 -0700

    simple todo list

---
+++ index.ts
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let todos = [];
+let condition ="true";
+
+
+while(condition)
+{
+    let addTask = await inquirer.prompt
+    (
+        [
+            {
+                name:"todos",
+                type:"input",
+                message:"What do you want to add in your Todos ?"
+            },
+            {
+                name:"addMoreTodos",
+                type:"confirm",
+                message:"Do you want to add more todos ?",
+                default:"false"
+            }
+        ]
+    );
+    todos.push(addTask.todos)
+    condition = addTask.addMoreTodos
+    console.log(todos)
+    
+}
